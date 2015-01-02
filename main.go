6894821761d90ae64f2a15bf91c633612dcff87b
Justin Overfelt <joverfelt@gmail.com>
2015-01-01 22:51:06 -0500

    ensure that the file is closed regardless of error conditions

--- main.go
+++ main.go
@@ -13,33 +13,39 @@ func main() {
 	args := os.Args[1:]
 	if len(args) != 2 {
 		fmt.Println("Usage: md5check [file] [provided md5 sum]")
-		os.Exit(0)
+		os.Exit(1)
 	}
 
 	filename := args[0]
 	knownSum, err := hex.DecodeString(args[1])
 	if err != nil {
 		fmt.Println("Error decoding provided md5 hash", err)
+		os.Exit(1)
 	}
 
 	file, err := os.Open(filename)
-	defer file.Close()
 	if err != nil {
 		fmt.Println("Error reading input file: ", err)
-		os.Exit(0)
+		file.Close()
+		os.Exit(1)
 	}
 
 	sum := md5.New()
 	_, err = io.Copy(sum, file)
 	if err != nil {
 		fmt.Println("Error computing md5 sum of provided file: ", err)
+		file.Close()
+		os.Exit(1)
 	}
 
 	fileSum := sum.Sum(nil)
 
 	if bytes.Compare(knownSum, fileSum) != 0 {
 		fmt.Printf("MISMATCH! MD5 Sum of given file was: %x\n", fileSum)
+		file.Close()
+		os.Exit(1)
 	} else {
 		fmt.Println("OK")
+		file.Close()
 	}
 }
